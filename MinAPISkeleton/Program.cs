1c0c3293d2731a8aa43e36d514bdb69cddea175f
DRavnaas <doylerav@gmail.com>
2023-02-10 10:59:02 -0800

    initial skeleton, minimal api

---
+++ MinAPISkeleton/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// todo - add db context for mongodb
+
+// Inject swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+// .MapPost
+// .MapPut
+// .MapDelete
+
+app.Run();
+

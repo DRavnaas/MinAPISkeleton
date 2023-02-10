ba7b9f4bbfa3f2b737e4c19a506c809458214821
DRavnaas <doylerav@gmail.com>
2023-02-10 11:50:43 -0800

    added swagger and no-op crud

--- MinAPISkeleton/Program.cs
+++ MinAPISkeleton/Program.cs
@@ -1,18 +1,32 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-// todo - add db context for mongodb
+// todo - add db context for mongodb, add appropriate rest calls to api
 
 // Inject swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    // Don't use this for release builds, shows stack trace
+    app.UseDeveloperExceptionPage();
+}
+app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Get done");
 
-// .MapPost
-// .MapPut
-// .MapDelete
+app.MapPut("/", () => "Put done");
+
+app.MapPost("/", () => "Post done");
+
+app.MapDelete("/", () =>
+{
+    throw new NotImplementedException("delete not implemented");
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
